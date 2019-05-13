de2a7b1c8d2cd54f6ac6fa3e6832abb03e48d8ca
Justine Tincq <jtincq@gmail.com>
2019-05-13 11:45:59 +0200

    stupid coatch implemented

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,13 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    return @answer = "Great !" if @question == "I am going to work"
+    return @answer = "Silly question, get dressed and go to work!" if @question.include? "?"
+    return @answer = "I don't care, get dressed and go to work!"
+  end
+
+
+end
